186d27ed7062866b7d3cef790554beeb61601ad5
M4ss1ck <4ndyraul@gmail.com>
2022-07-04 19:23:57 -0400

    send update on join

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -6,13 +6,22 @@ interface MainScreenProps {
 }
 
 const MainScreen = (props: MainScreenProps) => {
-  const { players } = useC4Context()
+  const { game, players, playerAddr, playerName } = useC4Context()
+  const isIncluded = players.some((p) => p.addr === playerAddr)
+  const handleJoin = () => {
+    const text = playerName + " joined Connect4"
+    window.webxdc.sendUpdate({ payload: { game: game, players: players }, info: text }, text)
+  }
   return (
     <div className="mainscreen" >
       <h1>Connect Four</h1>
-      <p>This is the classic Connect Four game</p>
+      <p>Hi, {playerName}! This is the classic Connect Four game</p>
       {/* <p>Once there are two players, you could start playing</p> */}
       {players.length > 0 && <h2>Players</h2>}
+      <ul>
+        {players.map(player => <li>{player.name}</li>)}
+      </ul>
+      {(players.length < 2 && !isIncluded) && <button onClick={handleJoin}>Join</button>}
       {players.length === 2 && <button onClick={props.onClick} >Click to start</button>}
     </div>
   )
