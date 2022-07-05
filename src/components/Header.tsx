a3f8e0ff607a49fb28b2e995a8cc6811bda2ff07
M4ss1ck <4ndyraul@gmail.com>
2022-07-05 11:24:54 -0400

    fix player colors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,12 @@ const Header = () => {
   const { game, activePlayer, setActivePlayer, players } = useC4Context()
   useEffect(() => {
     setActivePlayer(game.state.playing)
-  }, [game.state.playing])
+  }, [game.playing])
   return (
-        <div className="header">
-            <div className={activePlayer.color === '#000' ? 'player1' : 'player2'}></div>
-            <h2>{activePlayer.color === '#000' ? players[0] ?? 'Player 1' : players[1] ?? 'Player 2'}&apos;s turn</h2>
-        </div>
+    <div className="header">
+      <div className={activePlayer.color === '#000' ? 'player1' : 'player2'}></div>
+      <h2>{activePlayer.color === players[0].color ? players[0].name ?? 'Player 1' : players[1].name ?? 'Player 2'}&apos;s turn</h2>
+    </div>
   )
 }
 
