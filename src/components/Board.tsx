2277b8c126b025edf7c97aabd6ef8c27d54ecf85
M4ss1ck <4ndyraul@gmail.com>
2022-07-05 16:25:54 -0400

    remove clg

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -10,27 +10,22 @@ const Board = () => {
       {({ playerAddr, players, gameArray, setGameArray, moves, setMoves, setActivePlayer }) => {
         const game = new Connect4([new Player('#000'), new Player('#FFF')])
         moves.map(move => game.insert(move))
-        // setActivePlayer(game.state.playing)
-        // console.log(game.state)
         return <>
           <Header blur={!!game.state.winner} />
           <section className={game.state.winner ? 'board blur' : 'board'}>
             {gameArray.map((cell, index) => {
               return <Ball key={index} onClick={() => {
                 const col = game.getColumnByIndex(index)
-                // console.log("current col is " + col)
                 const isPlaying = players.some((p) => p.addr === playerAddr)
                 const isTurn = isPlaying ? game.state.playing.color === players.find(p => p.addr === playerAddr)?.color : false
 
                 if (isTurn && game.insert(col)) {
-                  console.log('se pudo jugar', game)
                   const newMoves = [...moves, col]
                   setMoves(newMoves)
                   const nextPlayer = players.find(p => p.addr !== playerAddr)
                   const text = "It's " + nextPlayer?.name + " turn in Connect4"
                   window.webxdc.sendUpdate({ payload: { move: col, moves: newMoves, players }, info: text }, text)
                   setGameArray(game.state.board)
-                  setActivePlayer(game.state.playing)
                 }
               }}>{game.state.board[index] === null ? <div></div> : <div className={game.state.board[index].color === '#000' ? 'player1' : 'player2'}></div>}</Ball>
             })}
--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -4,10 +4,8 @@ const MainScreen = (props: MainScreenProps) => {
   const { players, setPlayers, playerAddr, playerName } = useC4Context()
   const isIncluded = players.some((p) => p.addr === playerAddr)
   const handleJoin = () => {
-    console.log(players)
     const newPlayers = [...players, { name: playerName, addr: playerAddr, color: players.length === 0 ? '#000' : '#FFF' }]
     setPlayers(newPlayers)
-    console.log(newPlayers)
     const text = playerName + " joined Connect4"
     window.webxdc.sendUpdate({ payload: { move: -1, moves: [], players: newPlayers }, info: text }, text)
   }
@@ -15,7 +13,6 @@ const MainScreen = (props: MainScreenProps) => {
     <div className="mainscreen" >
       <h1>Connect Four</h1>
       <p>Hi, {playerName}! This is the classic Connect Four game</p>
-      {/* <p>Once there are two players, you could start playing</p> */}
       {players.length > 0 && <h2>Players</h2>}
       <ul>
         {players.map(player => <li key={player.addr}>{player.name}</li>)}
