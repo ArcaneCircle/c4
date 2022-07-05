dfe5422cf681a8902ec19738aecde5e94e34d098
M4ss1ck <4ndyraul@gmail.com>
2022-07-05 11:33:52 -0400

    send current move on click

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -5,18 +5,25 @@ import { C4Context } from '~/context/C4Context'
 const Board = () => {
   return (
     <C4Context.Consumer>
-      {({ game, gameArray, setGameArray }) => {
-        // console.log(gameArray.length)
+      {({ game, playerAddr, players, gameArray, setGameArray }) => {
+        // console.log(game.state)
         return <>
           <section className={game.state.winner ? 'board blur' : 'board'}>
             {gameArray.map((cell, index) => {
               return <Ball key={index} onClick={() => {
                 const col = game.getColumnByIndex(index)
-                console.log(col)
-                if (game.insert(col))
-                  console.log('se pudo jugar\n', game.state.board)
-                setGameArray(game.state.board)
-              }}>{gameArray[index] === null ? <div></div> : <div className={gameArray[index].color === '#000' ? 'player1' : 'player2'}></div>}</Ball>
+                // console.log("current col is " + col)
+                const isPlaying = players.some((p) => p.addr === playerAddr)
+                const isTurn = isPlaying ? game.state.playing.color === players.find(p => p.addr === playerAddr)?.color : false
+
+                if (isTurn && game.insert(col)) {
+                  console.log('se pudo jugar', game)
+                  const nextPlayer = players.find(p => p.addr !== playerAddr)
+                  const text = "It's " + nextPlayer?.name + " turn in Connect4"
+                  window.webxdc.sendUpdate({ payload: { move: col, players }, info: text }, text)
+                  setGameArray(game.state.board)
+                }
+              }}>{game.state.board[index] === null ? <div></div> : <div className={game.state.board[index].color === '#000' ? 'player1' : 'player2'}></div>}</Ball>
             })}
           </section>
           {game.state.winner && <div className="winner">{game.state.winner.color === '#000' ? 'Ganó el jugador 1' : 'Ganó el jugador 2'}</div>}
