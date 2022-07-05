bac7e403368310c2c9668035f05cdc084a23cb3d
M4ss1ck <4ndyraul@gmail.com>
2022-07-05 15:47:35 -0400

    add header to Board component

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,16 +1,19 @@
 /* eslint-disable no-console */
 import Ball from './Ball'
+import Header from './Header'
 import { Connect4, Player } from 'connect4-engine'
 import { C4Context } from '~/context/C4Context'
 
 const Board = () => {
   return (
     <C4Context.Consumer>
-      {({ playerAddr, players, gameArray, setGameArray, moves, setMoves }) => {
+      {({ playerAddr, players, gameArray, setGameArray, moves, setMoves, setActivePlayer }) => {
         const game = new Connect4([new Player('#000'), new Player('#FFF')])
         moves.map(move => game.insert(move))
+        // setActivePlayer(game.state.playing)
         // console.log(game.state)
         return <>
+          <Header blur={!!game.state.winner} />
           <section className={game.state.winner ? 'board blur' : 'board'}>
             {gameArray.map((cell, index) => {
               return <Ball key={index} onClick={() => {
@@ -27,6 +30,7 @@ const Board = () => {
                   const text = "It's " + nextPlayer?.name + " turn in Connect4"
                   window.webxdc.sendUpdate({ payload: { move: col, moves: newMoves, players }, info: text }, text)
                   setGameArray(game.state.board)
+                  setActivePlayer(game.state.playing)
                 }
               }}>{game.state.board[index] === null ? <div></div> : <div className={game.state.board[index].color === '#000' ? 'player1' : 'player2'}></div>}</Ball>
             })}
