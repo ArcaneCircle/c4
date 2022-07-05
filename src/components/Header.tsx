19313ae97061e721ad9a4dcf6741fecc0be05a68
M4ss1ck <4ndyraul@gmail.com>
2022-07-05 15:47:17 -0400

    add conditional blur to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 import { useC4Context } from '~/context/C4Context'
 
-const Header = () => {
+const Header = (props: HeaderProps) => {
   const { activePlayer, players } = useC4Context()
   return (
-    <div className="header">
+    <div className={props.blur ? "header blur" : "header"}>
       <div className={activePlayer.color === '#000' ? 'player1' : 'player2'}></div>
       <h2>{activePlayer.color === players[0].color ? players[0].name ?? 'Player 1' : players[1].name ?? 'Player 2'}&apos;s turn</h2>
     </div>
