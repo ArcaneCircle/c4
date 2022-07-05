5cc0dbe208802166be6b5aaed93827345789438a
M4ss1ck <4ndyraul@gmail.com>
2022-07-05 16:56:44 -0400

    modify header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,10 @@ const Header = (props: HeaderProps) => {
   const { players, moves } = useC4Context()
   const isFirstPlayerTurn = moves.length % 2 === 0
   return (
-    <div className={props.blur ? "header blur" : "header"}>
+    <header className={props.blur ? "blur" : ""}>
       <div className={isFirstPlayerTurn ? 'player1' : 'player2'}></div>
       <h2>{isFirstPlayerTurn ? players[0].name ?? 'Player 1' : players[1].name ?? 'Player 2'}&apos;s turn</h2>
-    </div>
+    </header>
   )
 }
 
