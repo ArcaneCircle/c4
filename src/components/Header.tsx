764ac817e6dd7083b8a3d9d6cd321746172497d0
M4ss1ck <4ndyraul@gmail.com>
2022-07-05 17:34:55 -0400

    set different text when it's your turn

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 import { useC4Context } from '~/context/C4Context'
 
 const Header = (props: HeaderProps) => {
-  const { players, moves } = useC4Context()
+  const { players, moves, playerAddr } = useC4Context()
   const isFirstPlayerTurn = moves.length % 2 === 0
   return (
     <header className={props.blur ? "blur" : ""}>
       <div className={isFirstPlayerTurn ? 'player1' : 'player2'}></div>
-      <h2>{isFirstPlayerTurn ? players[0].name ?? 'Player 1' : players[1].name ?? 'Player 2'}&apos;s turn</h2>
+      <h2>{isFirstPlayerTurn ? players[0].addr === playerAddr ? 'Your' : players[0].name + '\'s' : players[1].addr === playerAddr ? 'Your' : players[1].name + '\'s'} turn</h2>
     </header>
   )
 }
