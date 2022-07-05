a2b18215d1d8e590390c16dbb171801344b3d5f9
M4ss1ck <4ndyraul@gmail.com>
2022-07-05 16:25:34 -0400

    fix active player change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
 import { useC4Context } from '~/context/C4Context'
 
 const Header = (props: HeaderProps) => {
-  const { activePlayer, players } = useC4Context()
+  const { players, moves } = useC4Context()
+  const isFirstPlayerTurn = moves.length % 2 === 0
   return (
     <div className={props.blur ? "header blur" : "header"}>
-      <div className={activePlayer.color === '#000' ? 'player1' : 'player2'}></div>
-      <h2>{activePlayer.color === players[0].color ? players[0].name ?? 'Player 1' : players[1].name ?? 'Player 2'}&apos;s turn</h2>
+      <div className={isFirstPlayerTurn ? 'player1' : 'player2'}></div>
+      <h2>{isFirstPlayerTurn ? players[0].name ?? 'Player 1' : players[1].name ?? 'Player 2'}&apos;s turn</h2>
     </div>
   )
 }
