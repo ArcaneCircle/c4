dcad90dc7a35f21dc8d57321a28ff8e8dff1209f
M4ss1ck <4ndyraul@gmail.com>
2022-07-05 01:26:54 -0400

    add update listener

--- src/Game.tsx
+++ src/Game.tsx
@@ -3,10 +3,23 @@
 import Board from './components/Board'
 import Header from './components/Header'
 import MainScreen from './components/MainScreen'
+// import { Connect4 } from 'connect4-engine'
+import { useC4Context } from '~/context/C4Context'
+import { ReceivedStatusUpdate } from 'webxdc'
 
 export const Game: React.FC<{}> = () => {
-  const [started, setStarted] = useState(false)
+  const { setGameArray } = useC4Context()
+  useEffect(() => {
+    console.log('useEffect triggered')
+    window.webxdc.setUpdateListener((update: ReceivedStatusUpdate<C4Update>) => {
+      const { game } = update.payload
+      if (game) {
+        setGameArray(game.state.board)
+      }
+    })
+  }, [])
 
+  const [started, setStarted] = useState(false)
   return (
     <>
       <div className="mycontainer">
