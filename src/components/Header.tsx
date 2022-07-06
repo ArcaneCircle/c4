86ebb16f19a789317194100c9d91696a554be587
M4ss1ck <4ndyraul@gmail.com>
2022-07-06 02:12:49 -0400

    tweak header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = (props: HeaderProps) => {
   return (
     <header className={props.blur ? "blur" : ""}>
       <div className={isFirstPlayerTurn ? 'player1' : 'player2'}></div>
-      <h2 className={isFirstPlayerTurn ? 'red' : 'yellow'}>{isFirstPlayerTurn ? players[0].addr === playerAddr ? 'Your' : players[0].name + '\'s' : players[1].addr === playerAddr ? 'Your' : players[1].name + '\'s'} turn</h2>
+      <h2 >{isFirstPlayerTurn ? players[0].addr === playerAddr ? 'Your' : players[0].name + '\'s' : players[1].addr === playerAddr ? 'Your' : players[1].name + '\'s'} turn</h2>
     </header>
   )
 }
