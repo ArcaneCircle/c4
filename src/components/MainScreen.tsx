d6123c78cdb2f149b6a07af1663f5c9d689f4bba
M4ss1ck <4ndyraul@gmail.com>
2022-07-05 20:25:19 -0400

    change UI

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -11,14 +11,18 @@ const MainScreen = (props: MainScreenProps) => {
   }
   return (
     <div className="mainscreen" >
+      <span className='player1'></span>
       <h1>Connect Four</h1>
       <p>Hi, {playerName}! This is the classic Connect Four game</p>
-      {players.length > 0 && <h2>Players</h2>}
-      <ul>
-        {players.map(player => <li key={player.addr}>{player.name}</li>)}
-      </ul>
+      {players.length > 0 && <>
+        <h2>Players:</h2>
+        <ul>
+          {players.map(player => <li key={player.addr}>{player.name}</li>)}
+        </ul>
+      </>}
       {(players.length < 2 && !isIncluded) && <button onClick={handleJoin}>Join</button>}
       {players.length === 2 && <button onClick={props.onClick} >Click to start</button>}
+      <span className='player2'></span>
     </div>
   )
 }
