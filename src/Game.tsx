0173336d0785c79c4b6101a75acc9ef30540cfbd
M4ss1ck <4ndyraul@gmail.com>
2022-07-12 23:32:10 -0400

    fix update handling

--- src/Game.tsx
+++ src/Game.tsx
@@ -4,19 +4,26 @@ import { useC4Context } from '~/context/C4Context'
 import { ReceivedStatusUpdate } from 'webxdc'
 
 export const Game: React.FC<{}> = () => {
-  const { setPlayers, players, moves, setMoves, setLastMove } = useC4Context()
+  const { setPlayers, players, setMoves, setLastMove, playerAddr } = useC4Context()
   const [started, setStarted] = useState(false)
 
   useEffect(() => {
     window.webxdc.setUpdateListener((update: ReceivedStatusUpdate<C4Update>) => {
       if (update.serial && update.max_serial && update.serial === update.max_serial) {
         const { move: newMove, moves: newMoves, players: newPlayers } = update.payload
-        if (newMoves && (moves.length < newMoves.length || newMoves.length === 0)) {
-          setMoves(newMoves)
-          setLastMove(newMove)
-          if (players.length === newPlayers.length && players[0].addr !== newPlayers[0].addr) setPlayers(newPlayers)
+        setMoves(newMoves)
+        setLastMove(newMove)
+        if (update.payload.type) {
+          setPlayers(newPlayers)
+        } else if (newPlayers.length === 1 && newPlayers[0].addr === playerAddr) {
+          const summary = newPlayers[0].name + " is waiting for another player"
+          window.webxdc.sendUpdate({ payload: { move: -1, moves: [], players: newPlayers, type: "create" }, info: summary, summary }, summary)
+          setPlayers(newPlayers)
+        } else if (newPlayers.length === 2 && newPlayers[0].addr === playerAddr) {
+          const summary = newPlayers[0].name + " vs " + newPlayers[1].name
+          window.webxdc.sendUpdate({ payload: { move: -1, moves: [], players: newPlayers, type: "join" }, info: summary, summary }, summary)
+          setPlayers(newPlayers)
         }
-        if (players.length < newPlayers.length) setPlayers(newPlayers)
       }
     })
   }, [])
