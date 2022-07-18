3acc63cbf5b80222d8f454f182fbdded54606c12
M4ss1ck <4ndyraul@gmail.com>
2022-07-18 00:07:26 -0400

    add handleSendAgain function

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -2,7 +2,7 @@ import { useC4Context } from '~/context/C4Context'
 
 
 const Status = (props: StatusProps) => {
-    const { players, playerAddr, setMoves, setPlayers, setLastMove } = useC4Context();
+    const { players, playerAddr, setMoves, setPlayers, setLastMove, moves, lastMove } = useC4Context();
     const isPlaying = players.some((p) => p.addr === playerAddr)
     const isTurn = isPlaying ? props.color === players.find(p => p.addr === playerAddr)?.color : false
     const handleSurrender = () => {
@@ -26,12 +26,15 @@ const Status = (props: StatusProps) => {
             window.webxdc.sendUpdate({ payload: { move: -1, moves: [] as number[], players: newPlayers }, info: text, summary: summary }, text)
         }
     }
+    const handleSendAgain = () => {
+        window.webxdc.sendUpdate({ payload: { move: lastMove, moves: moves, players: players } }, "Sending state again")
+    }
     return (
         <section className={props.blur ? 'status blur' : 'status'}>
             <p className="font-bold"><span className='p1icon h-4 w-4 inline-flex'></span> {players[0].name} ({players[0].won})</p>
             <p className='italic'>vs</p>
             <p className='font-bold'><span className='p2icon h-4 w-4 inline-flex'></span> {players[1].name} ({players[1].won})</p>
-            {isPlaying && isTurn ? <button className='btn uppercase' onClick={handleSurrender}>Surrender</button> : <button className='btn uppercase'>Send Again</button>}
+            {isPlaying && <button className='btn uppercase' onClick={isTurn ? handleSurrender : handleSendAgain}>{isTurn ? "Surrender" : "Send Again"}</button>}
         </section>
     )
 }
